0d5df573197f871244c9f2a37cd1b02202046ff3
Fahridanaa <fahridanaaa@gmail.com>
2024-11-26 22:51:32 +0700

    fix: move shop request to folder Shop

--- app/Http/Requests/Shop/CreateShopRequest.php
+++ app/Http/Requests/Shop/CreateShopRequest.php
@@ -17,9 +17,9 @@ class CreateShopRequest extends FormRequest
             'user_id' => 'required|exists:users,id',
             'name' => 'required|string|max:255',
             'address' => 'required|string|max:500',
-            'description' => 'nullable|string|max:1000',
+            'description' => 'sometimes|string|max:1000',
             'operational' => 'required|string|max:255',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif|max:2048',
         ];
     }
 }
